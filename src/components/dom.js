1787fdc2bd055afcd688018db004d4a2f914fcb3
Ubuntu <ubuntu@ip-172-31-30-105.us-east-2.compute.internal>
2020-09-25 08:26:10 +0000

    Add height of the form.

--- src/components/dom.js
+++ src/components/dom.js
@@ -6,6 +6,8 @@ weatherDiv.id = 'weather-details';
 // weatherDiv.className = 'text-center weather d-flex flex-column justify-content-center';
 const title = document.createElement('p');
 title.className = 'display-2 font-weight-bold w-50 text-info';
+const temp = document.createElement('p');
+temp.className = 'font-weight-bold text-dark';
 const back = document.createElement('button');
 back.innerText = 'Go Back';
 back.className = 'btn btn-success btn-lg';
@@ -21,6 +23,7 @@ export default (data) => {
   const errTag = document.getElementsByClassName('err');
   const errArr = [...errTag];
   title.innerText = weatherMessage(data.weather[0].main, data.name);
+  temp.innerText = `With temperatures of ${Math.round(data.main.temp - 274.15)} degrees celcius.`;
   const condition = data.weather[0].main.toLowerCase();
   weatherDiv.className = `${condition} text-center weather d-flex flex-column justify-content-center align-items-center`;
   if (errArr.length > 0) {
@@ -30,6 +33,7 @@ export default (data) => {
   }
   form.remove();
   weatherDiv.appendChild(title);
+  weatherDiv.appendChild(temp);
   weatherDiv.appendChild(back);
   return content.appendChild(weatherDiv);
 };
